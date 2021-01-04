f104ba49fff8325a81debdcdd6979b5deb336c09
murash4 <singleant@gmail.com>
2021-01-04 16:59:13 +0300

    add content for home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,27 @@
+import { Fragment } from 'react'
+import { Search } from '../components/Search'
+import { Card } from '../components/Card'
+
 const Home = () => {
+	const cards = new Array(15)
+		.fill('')
+		.map((_, i) => i)
+
 	return (
-		<div>
-			<h1>Home page</h1>
-		</div>
+		<Fragment>
+			<Search />
+
+			<div className="row">
+				{ cards.map(card => (
+					<div
+						key={ card }
+						className="col-sm-4 mb-4"
+					>
+						<Card />
+					</div>
+				)) }
+			</div>
+		</Fragment>
 	)
 }
 
