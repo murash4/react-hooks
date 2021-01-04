32af2226a8f2cc4a684816b9d070a19e5c4e757c
murash4 <singleant@gmail.com>
2021-01-04 16:28:28 +0300

    add navigation and pages

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+const Navbar = () => {
+	return (
+		<nav className="navbar navbar-dark bg-primary navbar-expand-lg">
+			<div className="navbar-brand">
+				Gihub Поиск
+			</div>
+			<ul className="navbar-nav">
+				<li className="nav-item">
+					<a href="/" className="nav-link">Главная</a>
+				</li>
+				<li className="nav-item">
+					<a href="/about" className="nav-link">Информация</a>
+				</li>
+			</ul>
+		</nav>
+	)
+}
+
+export default Navbar
