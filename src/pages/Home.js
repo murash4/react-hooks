a04a2a9044c0b730948b6c520e8c355f5c40d743
murash4 <singleant@gmail.com>
2021-01-06 02:11:04 +0300

    modify home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,27 @@
-import { Fragment } from 'react'
+import {Fragment, useContext} from 'react'
 import { Search } from '../components/Search'
 import { Card } from '../components/Card'
+import {GithubContext} from "../context/github/githubContext";
 
 const Home = () => {
-	const cards = new Array(15)
-		.fill('')
-		.map((_, i) => i)
+	const { loading, users } = useContext(GithubContext)
 
 	return (
 		<Fragment>
 			<Search />
 
 			<div className="row">
-				{ cards.map(card => (
-					<div
-						key={ card }
-						className="col-sm-4 mb-4"
-					>
-						<Card />
-					</div>
-				)) }
+				{loading
+					? <p className="text-center">Загрузка...</p>
+					: users.map(user => (
+							<div
+								key={ user.id }
+								className="col-sm-4 mb-4"
+							>
+								<Card user={ user } />
+							</div>
+						))
+				}
 			</div>
 		</Fragment>
 	)
