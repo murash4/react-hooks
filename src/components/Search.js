f5f180464b67ac30c698cde92a0b41d183437f56
murash4 <singleant@gmail.com>
2021-01-05 14:45:03 +0300

    refinement of the search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,18 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { AlertContext } from '../context/alert/alertContext'
 
 export const Search = () => {
+	const [ value, setValue ] = useState('')
 	const { show } = useContext(AlertContext)
 	const onSubmit = (event) => {
-		if (event.key === 'Enter') {
-			show('Это Alert')
+		if (event.key !== 'Enter') {
+			return
+		}
+
+		if (value.trim()) {
+			console.log(`Ввели данные: ${value}`)
+		} else {
+			show('Введите данные пользователя!')
 		}
 	}
 
@@ -15,6 +22,8 @@ export const Search = () => {
 				type="text"
 				className="form-control"
 				placeholder="Введите ник пользователя..."
+				value={ value }
+				onChange={ event => setValue(event.target.value) }
 				onKeyPress={ onSubmit }
 			/>
 		</div>
