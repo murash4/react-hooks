76d0b6d1a8b288262d40384750d5405c6d0654d9
murash4 <singleant@gmail.com>
2021-01-05 15:06:42 +0300

    create githubState and githubReducer

--- src/App.js
+++ src/App.js
@@ -6,34 +6,37 @@ import About from './pages/About'
 import Profile from './pages/Profile'
 import { Alert } from './components/Alert'
 import { AlertState } from './context/alert/alertState'
+import { GithubState } from './context/github/GithubState'
 
 function App() {
   return (
-    <AlertState>
-      <BrowserRouter>
-        <Navbar />
-        <div className="container pt-4">
-          <Alert alert={{text: 'Test alert'}} />
-          <Switch>
-            <Route
-              path="/"
-              exact
-              component={ Home }
-            />
+    <GithubState>
+      <AlertState>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container pt-4">
+            <Alert alert={{text: 'Test alert'}} />
+            <Switch>
+              <Route
+                path="/"
+                exact
+                component={ Home }
+              />
 
-            <Route
-              path="/about"
-              component={ About }
-            />
+              <Route
+                path="/about"
+                component={ About }
+              />
 
-            <Route
-              path="/profile/:name"
-              component={ Profile }
-            />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    </AlertState>
+              <Route
+                path="/profile/:name"
+                component={ Profile }
+              />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </AlertState>
+    </GithubState>
   )
 }
 
